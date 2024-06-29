452341a8a48c7f383e5c443306ec655e8aa617c1
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-29 18:59:14 +0530

    29th June 2024

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const http = require("http");
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use('/', express.static(__dirname + "/public"));
 
