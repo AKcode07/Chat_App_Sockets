9fe711c01b13fc80e438394694bd61da32515fe7
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-30 13:20:27 +0530

    30th June 2024

--- index.js
+++ index.js
@@ -7,19 +7,34 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use('/', express.static(__dirname + "/public"));
 
 io.on("connection", (socket) => {
-  console.log("New connection is up...");
-  console.log(socket.id);
-  socket.on("msg_send", async (data) => {
-    console.log(data);
-    // const chat = await Chat.create({
-    //   roomId: data.roomid,
-    //   user: data.username,
-    //   content: data.msg,
-    // });
-    io.emit("msg_rcvd", data);
+  // console.log("New connection is up...");
+  // console.log(socket.id);
+  // socket.on("msg_send", async (data) => {
+  //   console.log(data);
+  //   // const chat = await Chat.create({
+  //   //   roomId: data.roomid,
+  //   //   user: data.username,
+  //   //   content: data.msg,
+  //   // });
+  //   io.emit("msg_rcvd", data);
+  // });
+});
+
+app.set("view engine", "ejs");
+
+app.use("/", express.static(__dirname + "/public"));
+
+app.get("/chat/:roomid", async (req, res) => {
+  const chats = await Chat.find({
+    roomId: req.params.roomid,
+  }).select("content user");
+  console.log(chats);
+  res.render("index", {
+    name: "Ashwin",
+    id: req.params.roomid,
+    chats: chats,
   });
 });
 
