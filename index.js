29d6a2317331d88d87196d05438ff34294334250
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-30 13:42:31 +0530

    30th June 2024

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketio = require('socket.io');
-
+const Chat = require("./models/chat");
 const connect = require('./config/database-config');
 const app = express();
 const server = http.createServer(app);
@@ -9,17 +9,24 @@ const io = socketio(server);
 
 
 io.on("connection", (socket) => {
-  // console.log("New connection is up...");
-  // console.log(socket.id);
-  // socket.on("msg_send", async (data) => {
-  //   console.log(data);
-  //   // const chat = await Chat.create({
-  //   //   roomId: data.roomid,
-  //   //   user: data.username,
-  //   //   content: data.msg,
-  //   // });
-  //   io.emit("msg_rcvd", data);
-  // });
+  socket.on("join_room", (data) => {
+    console.log("joining a room", data.roomid);
+    socket.join(data.roomid);
+  });
+
+  socket.on("msg_send", async (data) => {
+    console.log(data);
+    const chat = await Chat.create({
+      roomId: data.roomid,
+      user: data.username,
+      content: data.msg,
+    });
+    io.to(data.roomid).emit("msg_rcvd", data);
+  });
+
+  socket.on("typing", (data) => {
+    socket.broadcast.to(data.roomid).emit("someone_typing");
+  });
 });
 
 app.set("view engine", "ejs");
@@ -27,10 +34,10 @@ app.set("view engine", "ejs");
 app.use("/", express.static(__dirname + "/public"));
 
 app.get("/chat/:roomid", async (req, res) => {
-  // const chats = await Chat.find({
-  //   roomId: req.params.roomid,
-  // }).select("content user");
-  // console.log(chats);
+  const chats = await Chat.find({
+    roomId: req.params.roomid,
+  }).select("content user");
+  console.log(chats);
   res.render("index", {
     name: "Ashwin",
     id: req.params.roomid,
